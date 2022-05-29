83132f7ca27aa830760dcbefeb0eab317362aa97
Debian <bitnami@ip-172-26-9-178.eu-west-2.compute.internal>
2022-05-29 10:29:04 +0000

    cover styles

--- complete.php
+++ complete.php
@@ -3,7 +3,15 @@
 <head>
     <title>Shop Test</title>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
+    <style>
+        html {
+            padding: env(safe-area-inset-top) env(safe-area-inset-right) env(safe-area-inset-bottom) env(safe-area-inset-left);
+        }
+        body {
+            margin: 0;
+        }
+    </style>
 </head>
 <body>
     <h1>Complete</h1>
